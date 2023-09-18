30cbacc501e6e7c28be5fb78aac19823311fe4b1
ambco-iscte <ambco@iscte-iul.pt>
2023-09-18 13:30:04 +0100

    Fixed TSL. Organised packages.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.net.URI
 
 plugins {
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.8.20"
     id("maven-publish")
 }
 
