d9f670ed5ef53e336dc297ef0549139eeef09856
André L. Santos <andre.santos@iscte.pt>
2024-09-11 09:26:56 +0100

    test case for implicit casts: int->double char->int

---
+++ src/test/kotlin/pt/iscte/strudel/tests/javaparser/TestImplicitCasts.kt
@@ -0,0 +1,47 @@
+package pt.iscte.strudel.tests.javaparser
+
+import org.junit.jupiter.api.Test
+import pt.iscte.strudel.parsing.java.Java2Strudel
+import pt.iscte.strudel.model.*
+import pt.iscte.strudel.model.util.find
+import pt.iscte.strudel.model.util.findAll
+import pt.iscte.strudel.vm.IVirtualMachine
+import kotlin.math.min
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class TestImplicitCasts {
+    val code = """ 
+         class Test {
+            static double d() {
+                return 2;
+            }
+            
+            static int c() {
+                return 'a';
+            }
+         }
+    """.trimIndent()
+
+    @Test
+    fun testDoubleToInt() {
+        val vm = IVirtualMachine.create()
+        val module = Java2Strudel().load(code)
+        println(module)
+
+        val r = vm.execute(module["d"])
+        assertEquals(2.0, r?.toDouble())
+        assertEquals(DOUBLE, r?.type)
+    }
+
+    @Test
+    fun testCharToInt() {
+        val vm = IVirtualMachine.create()
+        val module = Java2Strudel().load(code)
+        println(module)
+
+        val r = vm.execute(module["c"])
+        assertEquals(97, r?.toInt())
+        assertEquals(INT, r?.type)
+    }
+}
